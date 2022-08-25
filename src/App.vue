7877f14db5b5aa8ee9219b4dd854187704c4313e
sherry <lemon19961007@sina.com>
2022-08-25 11:14:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="tabControl">
+      <router-link to="/labels">labels</router-link>
+      <span> | </span>
+      <router-link to="/money">money</router-link>
+      <span> | </span>
+      <router-link to="/statistics">statistics</router-link>
+      <hr>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    redirect: '/money'
+  },
+  {
+    path: '/money',
+    name: 'money',
+    component: Money
+  },
+  {
+    path: '/labels',
+    name: 'labels',
+    component: Labels
+  },
+  {
+    path: '/statistics',
+    name: 'statistics',
+    component: Statistics
+  },
+
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
