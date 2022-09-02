281dd96fd1cecebba79259beeb08233b38586801
sherry <lemon19961007@sina.com>
2022-09-02 09:57:57 +0800

    将store分为recordStore和tagStore

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,22 +1,9 @@
-import tagListModel from '@/models/tagListModel';
+import recordListModel from '@/models/recordListModel';
 
 const recordStore = {
-  // tag store
-  tagList: tagListModel.fetch(),
-  createTag: (name: string, icon: string) => {
-    const message = tagListModel.create(name, icon);
-    if (message === 'duplicated') {
-      window.alert('标签名已存在');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
-  },
-  updateTag: (id: string, name: string, icon: string) => {
-    return tagListModel.update(id, name, icon);
-  },
-  removeTag: (id: string) => {
-    return tagListModel.remove(id);
-  },
+  // record store
+  recordList: recordListModel.fetch(),
+  createRecord: (record: RecordItem) => recordListModel.create(record),
 };
 
 export default recordStore;
--- src/store/tagStore.ts
+++ src/store/tagStore.ts
@@ -1,8 +1,22 @@
-import recordListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 
 const tagStore = {
-  // record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
+  // tag store
+  tagList: tagListModel.fetch(),
+  createTag: (name: string, icon: string) => {
+    const message = tagListModel.create(name, icon);
+    if (message === 'duplicated') {
+      window.alert('标签名已存在');
+    } else if (message === 'success') {
+      window.alert('添加成功');
+    }
+  },
+  updateTag: (id: string, name: string, icon: string) => {
+    return tagListModel.update(id, name, icon);
+  },
+  removeTag: (id: string) => {
+    return tagListModel.remove(id);
+  },
+
 };
 export default tagStore;
